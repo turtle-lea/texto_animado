be7f2e52d56b446a365f49b6a40d78c61bc7fa4e
Poeta <poeta@poeta-RF510-RF410-RF710.(none)>
2013-08-28 12:54:18 -0300

    andaaa

--- foo.py
+++ foo.py
@@ -1,9 +1,13 @@
 from Tkinter import *
+from dameImagen import *
+from ejemplo import *
+from ttk import *
 
 win = Tk()
 win.title('TagCloud')
+app = Example(win)
 input_text = StringVar()
-
+prov = ImageProvider()
 entry = Entry(win, textvariable=input_text)
 entry.pack(fill='x', expand=True)
 
@@ -11,8 +15,12 @@ output = Label(win)
 output.pack(fill='both', expand=True)
 
 def update_output(*args):
-  d = ' | '.join(input_text.get().split())
-  output.config(text=d)
+  app.borrarHistorial()
+  d = input_text.get().split()
+  l = [prov.imageFor(i) for i in d]
+  for i in range(len(l)):
+	app.mostrarImagen(l[i],i)
+  output.config(text=l)
 
 input_text.trace_variable('w', update_output)
 
