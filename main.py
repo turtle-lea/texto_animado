23937c82ef4308a65b58f9a13b514246acb79ea8
Santiago Palladino <spalladino@manas.com.ar>
2013-08-28 14:34:20 -0300

    Text instead of entry

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
 from Tkinter import *
+from ScrolledText import *
 from dameImagen import *
 from ejemplo import *
 from ttk import *
 
 win = Tk()
 w, h = win.winfo_screenwidth(), win.winfo_screenheight()
-win.geometry("%dx%d+0+0" % (w, h))
-
-win.title('TagCloud')
-app = Example(win)
-input_text = StringVar()
-prov = ImageProvider()
-
-entry = Entry(win, textvariable=input_text)
-entry.pack(fill='x', expand=True)
-
-output = Label(win)
-output.pack(fill='both', expand=True)
+win.geometry("%dx%d+0+0" % (w,h))
 
 def update_output(*args):
 	app.borrarHistorial()
-	d = input_text.get().split()
+	d = entry.get(1.0, END).split()
 	l = [prov.imageFor(i) for i in d]
 	for i in range(len(l)):
 		app.mostrarImagen(l[i],i)
-		output.config(text=l)
 
-input_text.trace_variable('w', update_output)
+win.title('TagCloud')
+app = Example(win)
+prov = ImageProvider()
+
+entry = Text(win)
+entry.bind("<KeyPress>", update_output)
+entry.pack(fill='x', expand=True, anchor="n")
+
 win.mainloop()
